42c1040742918966082e62f32d7432c247d8f8bf
Kendy Ferreira De Oliveira <kendy619.kf@gmail.com>
2022-09-23 20:29:17 -0300

    ADD: INTERFACE ROLE AND  SERVICE ROLE.
    # Resolve 144

---
+++ Codigo/QueroJobs/Core/Services/IRoleService.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Services;
+public interface IRoleService
+{
+    Task<int> Create(Role role);
+    Task Edit(Role role);
+    Task Delete(int idRole);
+    Task<Role> Get(int idRole);
+    Task<IEnumerable<Role>> GetAll();
+}
