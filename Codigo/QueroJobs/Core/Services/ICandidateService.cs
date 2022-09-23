aa7de6012cad78ab39992e86937565fb1935d455
ericles <ericlesdsantos@gmail.com>
2022-09-23 12:46:39 -0300

    [Add] Add Service CandidateService

---
+++ Codigo/QueroJobs/Core/Services/ICandidateService.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Services;
+
+public interface ICandidateService
+{
+    Task<int> Create(Candidate candidate);
+    Task Edit(Candidate candidate);
+    Task Delete(int idCandidate);
+    Task<Candidate> Get(int idCandidate);
+    Task<IEnumerable<Candidate>> GetAll();
+}
