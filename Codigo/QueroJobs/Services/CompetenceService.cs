8171bf39c85a160bb876b5bb62fbfcb4dced52e5
Kendy Ferreira De Oliveira <kendy619.kf@gmail.com>
2022-09-24 15:06:45 -0300

    Caso de uso ManterCompetência implementado.
    # Resolve 141

---
+++ Codigo/QueroJobs/Services/CompetenceService.cs
@@ -0,0 +1,48 @@
+﻿using Core;
+using Core.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services;
+
+public class CompetenceService : ICompetenceService
+{
+    private readonly QueroJobsContext _queroJobsContext;
+
+    public CompetenceService(QueroJobsContext queroJobsContext)
+    {
+        _queroJobsContext = queroJobsContext;
+    }
+
+    public async Task<int> Create(Competence competence)
+    {
+        await _queroJobsContext.AddAsync(competence);
+        await _queroJobsContext.SaveChangesAsync();
+        return competence.Id;
+    }
+
+    public async Task Delete(int idCompetence)
+    {
+        var competence = await _queroJobsContext.Roles.FirstOrDefaultAsync(c => c.Id == idCompetence);
+
+        if (competence == null) return;
+
+        _queroJobsContext.Remove(competence);
+        await _queroJobsContext.SaveChangesAsync();
+    }
+
+    public async Task Edit(Competence competence)
+    {
+        _queroJobsContext.Update(competence);
+        await _queroJobsContext.SaveChangesAsync();
+    }
+
+    public async Task<Competence> Get(int idCompetence)
+    {
+        return await _queroJobsContext.Competences.FirstOrDefaultAsync(c => c.Id == idCompetence);
+    }
+
+    public async Task<IEnumerable<Competence>> GetAll()
+    {
+        return await _queroJobsContext.Competences.ToListAsync();
+    }
+}
