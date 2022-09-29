a792e008999ccc0b7f6b5f3f5fc1a9640e5628b4
xxthiagoboyxx <thiagoboy34@gmail.com>
2022-09-28 23:44:22 -0300

    Caso de uso implementado, no entanto ainda não está funcional.

---
+++ Codigo/QueroJobs/QueroJobsWEB/Controllers/CourseController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Core;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using QueroJobsWEB.Models;
+using Services;
+
+namespace QueroJobsWEB.Controllers;
+
+public class CourseController : Controller
+{
+    private readonly ICourseService _courseService;
+    private readonly IMapper _mapper;
+
+    public CourseController(ICourseService courseService, IMapper mapper)
+    {
+        _courseService = courseService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    /// <summary>
+    /// Return all courses in a ViewModel
+    /// </summary>
+    /// <returns>(courses)</returns>
+    public async Task<ActionResult> Index()
+    {
+        var courses = await _courseService.GetAll();
+
+        if (courses == null) return BadRequest();
+
+        var coursesModel = _mapper.Map<List<CourseModel>>(courses);
+
+        return View(coursesModel);
+    }
+
+    // GET: CourseController/Edit/5
+    [HttpGet]
+    public async Task<ActionResult> Edit(int id)
+    {
+        var course = await _courseService.Get(id);
+
+        var courseModel = _mapper.Map<CourseModel>(course);
+
+        return View(courseModel);
+    }
+
+    // POST: CompanyController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Edit(int id, CourseModel courseModel)
+    {
+        if (id != courseModel.Id) return NotFound();
+
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                var course = _mapper.Map<Course>(courseModel);
+                await _courseService.Edit(course);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(courseModel);
+
+    }
+
+    // GET: CourseController/Delete/5
+    [HttpGet]
+    public async Task<ActionResult> Delete(int? id)
+    {
+        if (id == null) return BadRequest();
+
+        var course = await _courseService.Get((int)id);
+
+        if (course == null) return NotFound();
+
+        var courseModel = _mapper.Map<CourseModel>(course);
+
+        return View(courseModel);
+    }
+
+    // POST: CourseController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _courseService.Delete(id);
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Codigo/QueroJobs/Services/CourseService.cs
@@ -0,0 +1,67 @@
+﻿using Core;
+using Core.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly QueroJobsContext _queroJobsContext;
+
+        public CourseService(QueroJobsContext queroJobsContext)
+        {
+            _queroJobsContext = queroJobsContext;
+        }
+
+        public async Task<int> Create(Course course)
+        {
+            await _queroJobsContext.AddAsync(course);
+            await _queroJobsContext.SaveChangesAsync();
+            return course.Id;
+        }
+
+        public async Task Delete(int idCourse)
+        {
+            var course = await _queroJobsContext.Companies.FirstOrDefaultAsync(c => c.Id == idCourse);
+
+            if (course == null) return;
+
+            _queroJobsContext.Remove(course);
+            await _queroJobsContext.SaveChangesAsync();
+
+        }
+
+        public async Task Edit(Course course)
+        {
+            _queroJobsContext.Update(course);
+
+            await _queroJobsContext.SaveChangesAsync();
+        }
+
+        public async Task<Course> Get(int idCourse)
+        {
+            return await _queroJobsContext.Courses.FirstOrDefaultAsync(c => c.Id == idCourse);
+        }
+
+        public async Task<IEnumerable<Course>> GetAll()
+        {
+            return await _queroJobsContext.Courses.ToListAsync();
+        }
+
+        public async Task UpdateCourseNameById(int id, string courseName)
+        {
+            var course = await _queroJobsContext.Courses.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (course == null) return;
+
+            course.CourseName = courseName;
+
+            await _queroJobsContext.SaveChangesAsync();
+        }
+    }
+}
