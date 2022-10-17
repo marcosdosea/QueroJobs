53917484ce2cee8081d01085c2c7e77308f18eaa
Joanne Stephany LabSI01 <joannestephany@hotmail.com>
2022-10-17 12:20:26 -0300

    //WIP
    Usei o modelo de company mas ainda está em produção a de Vacancy

--- Codigo/QueroJobs/QueroJobsWEBTests/Controllers/VacancyControllerTests.cs
+++ Codigo/QueroJobs/QueroJobsWEBTests/Controllers/VacancyControllerTests.cs
@@ -6,131 +6,294 @@ using QueroJobsWEB.Models;
 
 namespace QueroJobsWEB.Controllers;
 
+[TestClass()]
 public class VacancyController : Controller
 {
-    private readonly IVacancyService _vacancyService;
-    private readonly IMapper _mapper;
+    
+    private static VacancyController vacancyController;
 
-    public VacancyController(IVacancyService vacancyService, IMapper mapper)
+    [TestInitialize]
+    public void Initialize()
     {
-        _vacancyService = vacancyService;
-        _mapper = mapper;
+        // Arrange
+        var mockService = new Mock<IVacancyService>();
+
+        IMapper mapper = new MapperConfiguration(config => config.AddProfile(new VacancyProfile())).CreateMapper();
+
+        mockService.Setup(service => service.GetAll())
+                .Returns(GetTestCompanies());
+        mockService.Setup(service => service.Get(1))
+            .Returns(GetTargetVacancy());
+        mockService.Setup(service => service.Edit(It.IsAny<Vacancy>()))
+            .Verifiable();
+        mockService.Setup(service => service.Create(It.IsAny<Vacancy>()))
+            .Verifiable();
+
+        vacancyController = new VacancyController(mockService.Object, mapper);
     }
 
-
-    [HttpGet]
- 
-    public async Task<ActionResult> Index()
+   
+    [TestMethod()]
+    public void IndexTest()
     {
-        var vacancies = await _vacancyService.GetAll();
+        var result = vacancyController.Index().Result;
 
-        if (vacancies == null) return BadRequest();
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+        ViewResult viewResult = (ViewResult)result;
+        Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<VacancyModel>));
 
-        var vacanciesModel = _mapper.Map<List<VacancyModel>>(vacancies);
-
-        return View(vacanciesModel);
+        List<VacancyModel> list = (List<VacancyModel>)viewResult.ViewData.Model;
+        Assert.AreEqual(3, list.Count);
     }
 
-    // GET: VacancyController/Details/{id}
-    [HttpGet]
-    public async Task<ActionResult> Details(int id)
+    [TestMethod()]
+    public void DetailsTest()
     {
-        var vacancy = await _vacancyService.Get(id);
+        // Act
+        var result = vacancyController.Details(1).Result;
 
-        if (vacancy == null) return BadRequest();
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+        ViewResult viewResult = (ViewResult)result;
 
-        var vacancyModel = _mapper.Map<VacancyModel>(vacancy);
+        Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(VacancyModel));
+        VacancyModel vacancyModel = (VacancyModel)viewResult.ViewData.Model;
 
-        return View(vacancyModel);
+        Assert.AreEqual("ItatechJr", vacancyModel.FantasyName);
+        Assert.AreEqual("itatechjr@gmail.com", vacancyModel.Email);
     }
 
-
-    public ActionResult Create()
+    [TestMethod()]
+    public void CreateTest()
     {
-        return View();
+        // Act
+        var result = vacancyController.Create();
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
     }
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Create(VacancyModel vacancyModel)
+    [TestMethod()]
+    public void CreateTest_Valid()
     {
+        // Act
+        var result = vacancyController.Create(GetNewVacancy()).Result;
 
-        if (ModelState.IsValid)
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+        Assert.IsNull(redirectToActionResult.ControllerName);
+        Assert.AreEqual("Index", redirectToActionResult.ActionName);
+    }
+
+    [TestMethod()]
+    public void CreateTest_InValid()
+    {
+        // Arrange
+        vacancyController.ModelState.AddModelError("FantasyName", "Campo requerido");
+
+        // Act
+        var result = vacancyController.Create(GetNewVacancy()).Result;
+
+        // Assert
+        Assert.AreEqual(1, vacancyController.ModelState.ErrorCount);
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+        ViewResult viewResult = (ViewResult)result;
+        Assert.IsNotNull(viewResult);
+    }
+
+    [TestMethod()]
+    public void EditTest_Get()
+    {
+        // Act
+        var result = vacancyController.Edit(1).Result;
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+        ViewResult viewResult = (ViewResult)result;
+
+        Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(VacancyModel));
+        VacancyModel vacancyModel = (VacancyModel)viewResult.ViewData.Model;
+
+        Assert.AreEqual("ItatechJr", vacancyModel.FantasyName);
+        Assert.AreEqual("itatechjr@gmail.com", vacancyModel.Email);
+    }
+
+    [TestMethod()]
+    public void EditTest_Post()
+    {
+        // Act
+        var result = vacancyController.Edit(GetTargetVacancyModel().Id, GetTargetVacancyModel()).Result;
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+        Assert.IsNull(redirectToActionResult.ControllerName);
+        Assert.AreEqual("Index", redirectToActionResult.ActionName);
+    }
+
+    [TestMethod()]
+    public void DeleteTest_Post()
+    {
+        // Act
+        var result = vacancyController.Delete(1).Result;
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+        Assert.IsNull(redirectToActionResult.ControllerName);
+        Assert.AreEqual("Index", redirectToActionResult.ActionName);
+    }
+
+    [TestMethod()]
+    public void DeleteTest_Get()
+    {
+        // Act
+        var result = vacancyController.Delete(GetTargetVacancyModel().Id).Result;
+
+        // Assert
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+        Assert.IsNull(redirectToActionResult.ControllerName);
+        Assert.AreEqual("Index", redirectToActionResult.ActionName);
+    }
+
+    private VacancyModel GetNewVacancy()
+    {
+        return new VacancyModel
         {
-            try
-            {
-                var vacancy = _mapper.Map<Vacancy>(vacancyModel);
-                await _vacancyService.Create(vacancy);
-            }
-            catch
-            {
-                return View(vacancyModel);
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        return View(vacancyModel);
+            Id = 1,
+            FantasyName = "ItatechJr",
+            Email = "itatechjr@gmail.com",
+            Cep = "49504060",
+            Country = "Brasil",
+            State = "SE",
+            City = "Itabaiana",
+            District = "Marianga",
+            Street = "Rua Juca Monteiro",
+            HouseNumber = "1579",
+            Complement = "Ao lado do bar do Jegue",
+            CellphoneNumber = "79 999999999",
+            TelephoneNumber = null,
+            Cnpj = "10409486000170",
+            StateRegistration = null,
+            CorporateName = "SOFTWARES ITABAIANA JR. EMPRESA JUNIOR DO DEPARTAMENTO DE SISTEMAS DE INFORMACAO DA UFS",
+            ResponsibleName = "Ericles dos Santos"
+        };
 
     }
-
-
-    [HttpGet]
-    public async Task<ActionResult> Edit(int id)
+    private async Task<Vacancy> GetTargetVacancy()
     {
-        var vacancy = await _vacancyService.Get(id);
-
-        var vacancyModel = _mapper.Map<VacancyModel>(vacancy);
-
-        return View(vacancyModel);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Edit(int id, VacancyModel vacancyModel)
-    {
-        if (id != vacancyModel.Id) return NotFound();
-
-
-        if (ModelState.IsValid)
+        return new Vacancy
         {
-            try
-            {
-                var vacancy = _mapper.Map<Vacancy>(vacancyModel);
-                await _vacancyService.Edit(vacancy);
-            }
-            catch
-            {
-                return BadRequest();
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        return View(vacancyModel);
-
+            Id = 1,
+            FantasyName = "ItatechJr",
+            Email = "itatechjr@gmail.com",
+            Cep = "49504060",
+            Country = "Brasil",
+            State = "SE",
+            City = "Itabaiana",
+            District = "Marianga",
+            Street = "Rua Juca Monteiro",
+            HouseNumber = "1579",
+            Complement = "Ao lado do bar do Jegue",
+            CellphoneNumber = "79 999999999",
+            TelephoneNumber = null,
+            Cnpj = "10409486000170",
+            StateRegistration = null,
+            CorporateName = "SOFTWARES ITABAIANA JR. EMPRESA JUNIOR DO DEPARTAMENTO DE SISTEMAS DE INFORMACAO DA UFS",
+            ResponsibleName = "Ericles dos Santos"
+        };
     }
 
-    [HttpGet]
-    public async Task<ActionResult> Delete(int? id)
+    private VacancyModel GetTargetVacancyModel()
     {
-        if (id == null) return BadRequest();
-
-        var vacancy = await _vacancyService.Get((int)id);
-
-        if (vacancy == null) return NotFound();
-
-        var vacancyModel = _mapper.Map<VacancyModel>(vacancy);
-
-        return View(vacancyModel);
+        return new VacancyModel
+        {
+            Id = 1,
+            FantasyName = "ItatechJr",
+            Email = "itatechjr@gmail.com",
+            Cep = "49504060",
+            Country = "Brasil",
+            State = "SE",
+            City = "Itabaiana",
+            District = "Marianga",
+            Street = "Rua Juca Monteiro",
+            HouseNumber = "1579",
+            Complement = "Ao lado do bar do Jegue",
+            CellphoneNumber = "79 999999999",
+            TelephoneNumber = null,
+            Cnpj = "10409486000170",
+            StateRegistration = null,
+            CorporateName = "SOFTWARES ITABAIANA JR. EMPRESA JUNIOR DO DEPARTAMENTO DE SISTEMAS DE INFORMACAO DA UFS",
+            ResponsibleName = "Ericles dos Santos"
+        };
     }
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Delete(int id)
+    private async Task<IEnumerable<Vacancy>> GetTestCompanies()
     {
-        await _vacancyService.Delete(id);
-
-        return RedirectToAction(nameof(Index));
+        return new List<Vacancy>
+            {
+                new Vacancy
+                {
+                    Id = 1,
+                    FantasyName = "ItatechJr",
+                    Email = "itatechjr@gmail.com",
+                    Cep = "49504060",
+                    Country = "Brasil",
+                    State = "SE",
+                    City = "Itabaiana",
+                    District = "Marianga",
+                    Street = "Rua Juca Monteiro",
+                    HouseNumber = "1579",
+                    Complement = "Ao lado do bar do Jegue",
+                    CellphoneNumber = "79 999999999",
+                    TelephoneNumber = null,
+                    Cnpj = "10409486000170",
+                    StateRegistration = null,
+                    CorporateName = "SOFTWARES ITABAIANA JR. EMPRESA JUNIOR DO DEPARTAMENTO DE SISTEMAS DE INFORMACAO DA UFS",
+                    ResponsibleName = "Ericles dos Santos"
+                },
+            new Vacancy
+            {
+                Id = 2,
+                FantasyName = "Titan",
+                Email = "Titan@gmail.com",
+                Cep = "49500001",
+                Country = "Brasil",
+                State = "SE",
+                City = "Aracaju",
+                District = "Bananeira",
+                Street = "Belivaldo Chagas",
+                HouseNumber = "6458",
+                Complement = null,
+                CellphoneNumber = "78 888888888",
+                TelephoneNumber = null,
+                Cnpj = "39409486000504",
+                StateRegistration = null,
+                CorporateName = "TITAN SOFTWARES ARACAJU",
+                ResponsibleName = "Sinho senho"
+            },
+                new Vacancy
+                {
+                    Id = 3,
+                    FantasyName = "Fabrica de Softwares",
+                    Email = "fabricadesoftwares@gmail.com",
+                    Cep = "24050363",
+                    Country = "Brasil",
+                    State = "SP",
+                    City = "São Paulo",
+                    District = "Centro",
+                    Street = "Bloco A",
+                    HouseNumber = "01",
+                    Complement = null,
+                    CellphoneNumber = "79 999999999",
+                    TelephoneNumber = null,
+                    Cnpj = "11456548978914",
+                    StateRegistration = null,
+                    CorporateName = "FABRICA DE SOFTWARES",
+                    ResponsibleName = "Dosea"
+                },
+            };
     }
 }
