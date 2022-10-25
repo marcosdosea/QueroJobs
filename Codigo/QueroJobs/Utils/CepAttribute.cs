9519e867a8cf0365363cba7d7dbf7a9729bec5a7
Ericles Dos Santos <ericles.santos@inventione.com.br>
2022-10-25 17:45:44 -0300

    [Add] Utils and personalized data validations

---
+++ Codigo/QueroJobs/Utils/CepAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Utils;
+
+/// <summary>
+/// Validação customizada para CPF
+/// </summary>
+public class CepAttribute : ValidationAttribute
+{
+    /// <summary>
+    /// Construtor
+    /// </summary>
+    public CepAttribute() { }
+
+    /// <summary>
+    /// Validação server
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+#pragma warning disable CS8765 // A nulidade do tipo de parâmetro 'value' não corresponde ao membro substituído (possivelmente devido a atributos de nulidade).
+    public override bool IsValid(object value)
+#pragma warning restore CS8765 // A nulidade do tipo de parâmetro 'value' não corresponde ao membro substituído (possivelmente devido a atributos de nulidade).
+    {
+        if (value == null || string.IsNullOrEmpty(value.ToString()))
+            return true;
+        var valueNoEspecial = Methods.RemoveSpecialsCaracts((string)value);
+        if (valueNoEspecial.ToString().Length != 8)
+            return false;
+        if (valueNoEspecial.ToString().StartsWith("0"))
+            return false;
+        return true;
+    }
+
+    public string ErrorMessage =>
+        $"CEP Inválido";
+}
